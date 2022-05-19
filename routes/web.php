34a4b30a44f3aa950229b1aac3070e73225bb0ef
wscjdsn2008 <196381467@qq.com>
2022-05-19 15:15:11 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('', function () {
+    //return view('welcome');
+    return 'hhhh';
 });
+Route::get('/index', function () {
+    return 'hello 黄杭';
+});
+Route::get('/test1/{id}', function ($id) {
+    return "必须参数是:$id";
+});
+Route::get('/test2/{id?}', function ($id = '我就是个默认值') {
+    return "加了问号的可参数是:$id";
+});
+Route::get('test3/{id}', function ($id) {
+    return "单正则匹配$id..."; //约束的正则路由的参数
+})->where('id', '\d+');
+Route::get('test4/{name}/{age}', function ($n, $a) {
+    return "多参数正则匹配$n----$a...";
+})->where(['name' => '\w+', 'age' => '\d+']);
+Route::get('test5/fn', [TestController::class, 'lst']);
+Route::get('/test6/', function () {
+    return "路由别名";
+})->name('t6');
